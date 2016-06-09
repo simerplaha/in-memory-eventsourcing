ebcb02802ab7227a3b574c2e959b19609a1cc618
Simer P <simer.j@gmail.com>
2016-06-10 02:09:36 +1000

    Updated few dependencies

--- project/build.scala
+++ project/build.scala
@@ -1,4 +1,3 @@
-import com.typesafe.sbt.SbtAspectj._
 import sbt.Keys._
 import sbt._
 
@@ -21,31 +20,22 @@ object Settings {
 
   object versions {
     val akkaV = "2.4.7"
-
     val scala = "2.11.8"
-    val log4js = "1.4.10"
   }
-
-
 
   val allDependencies =
     Def.setting(Seq(
       "ch.qos.logback" % "logback-classic" % "1.1.3",
-      "com.github.mauricio" %% "postgresql-async" % "0.2.18",
       "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0",
       "org.scalatest" %% "scalatest" % "3.0.0-M10" % "test",
       "org.specs2" %% "specs2-common" % "2.3.11" % "test",
       "com.typesafe.slick" %% "slick" % "3.1.1",
       "com.typesafe.slick" %% "slick-hikaricp" % "3.1.1",
-      "org.postgresql" % "postgresql" % "9.4.1207.jre7",
-      "com.github.tminglei" %% "slick-pg" % "0.11.0",
-      "com.zaxxer" % "HikariCP" % "2.4.3",
+      "org.postgresql" % "postgresql" % "9.4.1208.jre7",
+      "com.github.tminglei" %% "slick-pg" % "0.14.1",
+      "com.zaxxer" % "HikariCP" % "2.4.6",
       "com.typesafe.akka" %% "akka-typed-experimental" % versions.akkaV,
-      "com.typesafe.akka" %% "akka-agent" % versions.akkaV,
-      "org.json4s" %% "json4s-native" % "3.2.10",
       "com.github.nscala-time" %% "nscala-time" % "2.8.0",
-      //      "org.scalaz" %% "scalaz-core" % "7.2.1",
-      "com.propensive" %% "rapture" % "2.0.0-M5",
       "io.argonaut" %% "argonaut" % "6.1",
       "com.github.alexarchambault" %% "argonaut-shapeless_6.1" % "1.0.0-RC1",
       "net.liftweb" %% "lift-json" % "3.0-M5-1",
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,6 +1,3 @@
-// repository for Typesafe plugins
 resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/"
 
 addSbtPlugin("io.spray" % "sbt-revolver" % "0.7.1")
-
-addSbtPlugin("com.typesafe.sbt" % "sbt-aspectj" % "0.10.4")
